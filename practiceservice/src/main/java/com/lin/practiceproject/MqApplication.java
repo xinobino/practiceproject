d38ce2d4b1f961b50e6d9561d7655bef195e5d98
lin.li09 <lin.li09@mljr.com>
2020-12-30 11:19:49 +0800

    11

--- practiceservice/src/main/java/com/lin/practiceproject/MqApplication.java
+++ practiceservice/src/main/java/com/lin/practiceproject/MqApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MqApplication {
 
     public static void main(String[] args) {
+        System.out.println("123");
         SpringApplication.run(MqApplication.class, args);
     }
 }
